8fe00553706ada2605ccd95eee74668a9c98353a
kru1910 <KrupaK@indusa.com>
2022-05-03 17:02:50 +0530

    Add: User and Book-Collection relation Description: User can access only authorized book-records

--- config/datastores.js
+++ config/datastores.js
@@ -12,50 +12,17 @@
  * For more information on configuring datastores, check out:
  * https://sailsjs.com/config/datastores
  */
+ const  host= 'localhost';
+ const  user= 'root';
+ const  password='Root@1910';
+ const  port= '3306';
+ const  database='books';
 
 module.exports.datastores = {
 
-
-  /***************************************************************************
-  *                                                                          *
-  * Your app's default datastore.                                            *
-  *                                                                          *
-  * Sails apps read and write to local disk by default, using a built-in     *
-  * database adapter called `sails-disk`.  This feature is purely for        *
-  * convenience during development; since `sails-disk` is not designed for   *
-  * use in a production environment.                                         *
-  *                                                                          *
-  * To use a different db _in development_, follow the directions below.     *
-  * Otherwise, just leave the default datastore as-is, with no `adapter`.    *
-  *                                                                          *
-  * (For production configuration, see `config/env/production.js`.)          *
-  *                                                                          *
-  ***************************************************************************/
-
   default: {
-
-    /***************************************************************************
-    *                                                                          *
-    * Want to use a different database during development?                     *
-    *                                                                          *
-    * 1. Choose an adapter:                                                    *
-    *    https://sailsjs.com/plugins/databases                                 *
-    *                                                                          *
-    * 2. Install it as a dependency of your Sails app.                         *
-    *    (For example:  npm install sails-mysql --save)                        *
-    *                                                                          *
-    * 3. Then pass it in, along with a connection URL.                         *
-    *    (See https://sailsjs.com/config/datastores for help.)                 *
-    *                                                                          *
-    ***************************************************************************/
-    adapter: 'sails-mysql',
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    port: '3306',
-    database: 'books'
-
+    adapter: "sails-mysql",
+    url: `mysql://${user}:${password}@${host}:${port}/${database}`
   },
-
 
 };
